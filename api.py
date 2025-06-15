0277cad3744dff0b860f26e405b3916be79e12ac
Juan Manuel Saad <juanmanuel.saad@hotmail.com>
2025-06-15 19:29:07 -0300

    Add root endpoint to fetch data from local API

--- api.py
+++ api.py
@@ -1,3 +1,9 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
+
+@app.get("/")
+async def root():
+    respuesta = requests.get("http://localhost:8001/")
+    return respuesta.json()
